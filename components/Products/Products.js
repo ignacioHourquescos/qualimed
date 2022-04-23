2cb69765391d212311e88b5ef79ccf7fe6abb317
Agustin Cordoba <agustin.cordoba.work@gmail.com>
2022-04-23 18:16:02 -0300

    Contact page

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,21 +1,38 @@
 import React from 'react'
 import Cards from '../Ui/Cards/Cards';
 import styles from './Products.module.scss';
-import Link from 'next/link'
+import { useFetch } from '../../hooks/useFetch';
 
 
 
 const Products = () => {
+
+  const { loading, data } =  useFetch(`https://qualimed.herokuapp.com/articulos`);
   return (
 
           
       <div className={styles.cards}>
-        <Cards img='/monitorSignosVitales.png' title="Monitor de signos vitales" description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore '/>
-        <Cards img='/monitorSignosVitales.png' title="Monitor de signos vitales" description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore '/>
-        <Cards img='/monitorSignosVitales.png' title="Monitor de signos vitales" description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore '/>
-        <Cards img='/monitorSignosVitales.png' title="Monitor de signos vitales" description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore '/>
-        <Cards img='/monitorSignosVitales.png' title="Monitor de signos vitales" description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore '/>
-        <Cards img='/monitorSignosVitales.png' title="Monitor de signos vitales" description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore '/>
+        {
+          loading  ? (
+          <>
+          <Cards img='/monitorSignosVitales.png' title="Monitor de signos vitales" description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore '/>
+          <Cards img='/monitorSignosVitales.png' title="Monitor de signos vitales" description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore '/>
+          <Cards img='/monitorSignosVitales.png' title="Monitor de signos vitales" description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore '/>
+          <Cards img='/monitorSignosVitales.png' title="Monitor de signos vitales" description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore '/>
+          <Cards img='/monitorSignosVitales.png' title="Monitor de signos vitales" description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore '/>
+          <Cards img='/monitorSignosVitales.png' title="Monitor de signos vitales" description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore '/>
+          </>
+          )
+          :
+          (
+          data.map(({id, description}) => ( 
+                    <Cards img='/monitorSignosVitales.png' title="Monitor de signos vitales" description={description}/>
+
+          
+          ))
+          )
+        }
+        
       </div>
       
 

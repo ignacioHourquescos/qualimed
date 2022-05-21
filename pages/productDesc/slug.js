85b3eaa38d32fb242181a56a9efde4f6f976f7e4
Agustin Cordoba <agustin.cordoba.work@gmail.com>
2022-05-21 20:24:49 -0300

    detalle de productos

--- pages/productDesc/slug.js
+++ pages/productDesc/slug.js
@@ -1,8 +1,26 @@
-import React from 'react';
 import Header from '/components/Header/Header';
 import Footer from '/components/Footer/Footer';
 import styles from "./slug.module.scss";
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper";
+import Cards from '../../components/Ui/Cards/Cards';
+
 const slug = () => {
+
+  const settings = {
+    className: "center",
+    centerMode: true,
+    infinite: false,
+    centerPadding: "60px",
+    slidesToShow: 3,
+    speed: 500
+  };
+
+  const description = "lorem";
+
   return (
     <div className={styles.contAll}>
       <Header />
@@ -80,10 +98,42 @@ Viverra aliquet eget sit amet tellus cras adipiscing enim eu. Senectus et netus 
                     <button>Cotización via whatsapp</button>
                     <button style={{background:'#8183CA'}}>Cotización via mail</button>
                 </div>
-                <div className={styles.shop}><img src='/cart.png'/>Ver producto en tienda minorista</div>
+                <div className={styles.shop}><img src='/cart2.png'/>Ver producto en tienda minorista</div>
             </div>
         </div>
-        <div className={styles.related}></div>
+        <div className={styles.related}>
+        <div className={styles.seccions}>
+            <h3>Productos relacionados</h3>
+              {/* {
+                loading &&
+                data.map(({id, description}) => (
+                  <div>
+                    <Cards img='/monitorSignosVitales.png' title="Monitor de signos vitales" description={description}/>
+                  </div>
+                    )
+                  )
+            
+              } */}
+            <div className={styles.swiper}>
+              <Swiper
+                      slidesPerView={"auto"}
+                      spaceBetween={30}
+                      modules={[Pagination]}
+                      
+                    >
+                      <SwiperSlide className={styles.swiperSlide}><Cards img='/monitorSignosVitales.png' title="Monitor de signos vitales" description={description}/></SwiperSlide>
+                      <SwiperSlide className={styles.swiperSlide}><Cards img='/monitorSignosVitales.png' title="Monitor de signos vitales" description={description}/></SwiperSlide>
+                      <SwiperSlide className={styles.swiperSlide}><Cards img='/monitorSignosVitales.png' title="Monitor de signos vitales" description={description}/></SwiperSlide>
+                      <SwiperSlide className={styles.swiperSlide}><Cards img='/monitorSignosVitales.png' title="Monitor de signos vitales" description={description}/></SwiperSlide>
+                      <SwiperSlide className={styles.swiperSlide}><Cards img='/monitorSignosVitales.png' title="Monitor de signos vitales" description={description}/></SwiperSlide>
+                      <SwiperSlide className={styles.swiperSlide}><Cards img='/monitorSignosVitales.png' title="Monitor de signos vitales" description={description}/></SwiperSlide>
+                      <SwiperSlide className={styles.swiperSlide}><Cards img='/monitorSignosVitales.png' title="Monitor de signos vitales" description={description}/></SwiperSlide>
+              
+                    </Swiper>
+            </div>
+          </div>
+
+        </div>
 
       </div>
 

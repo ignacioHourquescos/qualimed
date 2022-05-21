876d9419abad5beffc425c10f8512ef2890b522d
Jimancio <54536722+ignacioHourquescos@users.noreply.github.com>
2022-05-21 17:44:11 -0300

    agliunso cambios igna

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Header from '../../components/Header/Header'
 import Products from '../../components/Products/Products'
 import styles from '../products/index.module.scss'
 
+
 const index = () => {
+
+
+  useEffect(()=>{
+    fetch('https://sheets.googleapis.com/v4/spreadsheets/1CMfYFGhXhIBEMO-Ob9CZucRujqTdkRSIkD7hM-xaYew/values/MASTER?alt=json')
+
+    .then(response => console.log(response.json()))
+  },[])
+
+
+
   return (
     <>
       <Header/>

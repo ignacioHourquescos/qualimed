066387cb5044bd1e1619aa5d961376ac15414eec
Agustin Cordoba <agustin.cordoba.work@gmail.com>
2022-06-03 12:04:58 -0300

    slug reparado

--- pages/products/equipamiento/index.js
+++ pages/products/equipamiento/index.js
@@ -7,7 +7,6 @@ import CarrouselMobile from "../../../components/CarrouselMobile/CarrouselMobile
 import Filter from "../../../components/Filter/Filter";
 import Footer from '/components/Footer/Footer';
 
-<<<<<<< HEAD
 const index = () => {
 
   const {searchText} = Filter;
@@ -24,9 +23,6 @@ const index = () => {
     console.log("esta es la fucntion test " + e )
   }
 
-=======
-const Index = () => {
->>>>>>> fc43a4fa89c376b932af6e3774f15287c2bf42fa
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -52,23 +48,23 @@ const Index = () => {
       <div className={styles.container}>
         <Filter testFunction={testFunction}/>
         <div className={styles.products}>
-          {/* { 
-            searchText = "" ?
+          {/* {  */}
+            {/* searchText = "" ? */}
               <Products
               data={products.filter(
                 (element) => element.category == "EQUIPAMIENTO"
               )}
               loading={loading}
             />
-          :
-          <Products
-              data={products.filter(
-                (element) => element.title.toLowerCase() == searchText.toLowerCase()
-              )}
-              loading={loading}
-            />
+          {/* // :
+          // <Products
+          //     data={products.filter(
+          //       (element) => element.title.toLowerCase() == searchText.toLowerCase()
+          //     )}
+          //     loading={loading}
+          //   />
             
-          } */}
+          // } */}
         </div>
 
         <div className={styles.carrousel}>
@@ -82,4 +78,4 @@ const Index = () => {
   );
 };
 
-export default Index;
+export default index;

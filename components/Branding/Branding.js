c3d610e80db907aa083bbea0c2b008095843d35f
acordoba <aguscordoba11@hotmail.com>
2022-06-10 17:16:20 -0300

    branding img

--- components/Branding/Branding.js
+++ components/Branding/Branding.js
@@ -8,7 +8,7 @@ import {Autoplay } from "swiper";
 
 const Branding = () => {
 
-    const images= ['/brand1.png', '/brand2.png','/brand3.png','/brand4.png','/brand5.png','/brand6.png','/brand7.png','/brand8.png','/brand9.png','/brand10.png','/brand11.png','/brand12.png','/brand13.png','/brand14.png','/brand15.png'];
+  const images = ['/brand1.png', '/brand2.png','/brand3.png','/brand4.png','/brand5.png','/brand6.png','/brand7.png','/brand8.png','/brand9.png','/brand10.png','/brand11.png','/brand12.png','/brand13.png','/brand14.png','/brand15.png', '/brand16.png', '/brand17.png', , '/brand18.jpg'];
 
   return (
     <div className={styles.container}>

c73694a8d6d3b7701fca791f53c4a851044a9472
Jimancio <54536722+ignacioHourquescos@users.noreply.github.com>
2022-07-14 13:59:31 -0300

    funcionando hacekado

--- pages/productDesc/[slug].js
+++ pages/productDesc/[slug].js
@@ -20,11 +20,65 @@ const Slug = () => {
 	const [displayUserForm, setDisplayUserForm] = useState(false);
 	const [sending, setSending] = useState(false);
 
-	useEffect(() => {
-		fetch("../api/getProducts")
+	const getProducts = () => {
+		var array = [];
+		let categoriesArray = [];
+		let brandsArray = [];
+
+		let sheetName = "MASTER";
+		let fileCode = "1CMfYFGhXhIBEMO-Ob9CZucRujqTdkRSIkD7hM-xaYew"; //codigo de la derecha
+		let APIkey = "AIzaSyAQGQq6Vbh7blIY3J7XwzVrUBDe3tQelm8";
+
+		fetch(
+			`https://sheets.googleapis.com/v4/spreadsheets/${fileCode}/values/${sheetName}?alt=json&key=${APIkey}`
+		)
 			.then((response) => response.json())
-			.then((data) => (setProducts(data[0]), setLoading(false)));
+
+			.then((data) => {
+				for (var i = 1; i < data.values.length; i++) {
+					array.push({
+						code: data.values[i][0],
+						category: !data.values[i][1] ? "" : data.values[i][1],
+						brand: data.values[i][2],
+						title: data.values[i][3],
+						brief: data.values[i][4],
+						description: data.values[i][5],
+						application: data.values[i][6],
+						techcnial: data.values[i][7],
+						img: !data.values[i][8]
+							? "barbijo.png"
+							: "https://drive.google.com/uc?export=view&id=" +
+							  data.values[i][8],
+						ml: !data.values[i][9] ? "" : data.values[i][9],
+					});
+
+					categoriesArray.push(data.values[i][0]);
+					brandsArray.push(data.values[i][2]);
+				}
+				setProducts(array);
+				// res.send(
+				//   JSON.stringify([
+				//     array,
+				//     categoriesArray.filter((v, i, a) => a.indexOf(v) === i),
+				//     brandsArray.filter((v, i, a) => a.indexOf(v) === i),
+				//   ])
+				// );
+			})
+			.then((data) => {
+				console.log("aca desde usef ecct" + data);
+				// setProducts(data.values[0]);
+				setLoading(false);
+			});
+	};
+
+	useEffect(() => {
+		getProducts();
 	}, []);
+	// useEffect(() => {
+	// 	fetch("../api/getProducts")
+	// 		.then((response) => response.json())
+	// 		.then((data) => (setProducts(data[0]), setLoading(false)));
+	// }, []);
 
 	const sendEmail = (e) => {
 		e.preventDefault();

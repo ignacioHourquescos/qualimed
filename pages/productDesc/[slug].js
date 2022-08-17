e517f80c7517569ec566afb4a7f422bad0c61667
Jimancio <54536722+ignacioHourquescos@users.noreply.github.com>
2022-08-17 00:19:51 -0300

    agrego revalidate

--- pages/productDesc/[slug].js
+++ pages/productDesc/[slug].js
@@ -8,92 +8,71 @@ import Hero2 from "../../components/Hero2/Hero2";
 import emailjs from "@emailjs/browser";
 import ReCAPTCHA from "react-google-recaptcha";
 import Loader from "../../components/Ui/Loader/Loader";
-<<<<<<< HEAD
+import getProducts from "../api/getProducts";
+import Cotizacion from "../../components/Ui/Cotizacion/Cotizacion";
 
-const Index = () => {
-	// const {img, title, description, application, techcnial, code} = products;
+export const getStaticPaths = async () => {
+	const data = await getProducts();
+	const paths = data.map((product) => {
+		return {
+			params: {
+				slug:
+					product[0].toString().toLowerCase().trim() ||
+					"ECG80".toString().toLowerCase().trim(),
+			},
+		};
+	});
+
+	return {
+		paths,
+		fallback: false,
+	};
+};
+
+export const getStaticProps = async (context) => {
+	const slug = context.params.slug;
+	const data = await getProducts();
+	const arrayFiltered = data.find(
+		(element) => element[0].toString().toLowerCase().trim() == slug
+	);
+	return {
+		props: { data: arrayFiltered },
+	};
+};
+
+const Index = ({ data }) => {
 	const [loaded, setLoaded] = useState(false);
-	const form = useRef();
-	const router = useRouter();
-	const { slug } = router.query;
-	const [products, setProducts] = useState([]);
 	const [loading, setLoading] = useState(true);
-	const [displayUserForm, setDisplayUserForm] = useState(false);
-	const [sending, setSending] = useState(false);
+	const [products, setProducts] = useState(null);
 
 	useEffect(() => {
-		console.log("CODE PARAMS", slug);
-		const fetchData = async () => {
-			const response = await fetch("/api/getProducts", {
-				headers: {
-					"Content-Type": "application/json",
-					Accept: "application/json",
-				},
+		if (data) {
+			setProducts({
+				code: data[0],
+				category: !data[1] ? "" : data[1],
+				brand: data[2],
+				title: data[3],
+				brief: data[4],
+				description: data[5],
+				application: data[6],
+				techcnial: data[7],
+				img: !data[8]
+					? "barbijo.png"
+					: "https://drive.google.com/uc?export=view&id=" + data[8],
+				ml: !data[9] ? "" : data[9],
 			});
-			const data = await response.json();
-			console.log("DATA", data[0]);
-
-			const arrayFiltered = data[0].find((element) => element.code == slug);
-			console.log("DATA", arrayFiltered);
-			setProducts(arrayFiltered);
-		};
-		fetchData();
-		setLoading(false);
-	}, []);
-
-	const sendEmail = (e, captchaValue) => {
-		e.preventDefault();
-		setSending(true);
-		console.log("mensaje enviado");
-		emailjs
-			.sendForm(
-				"service_2qdstih",
-				"template_fldorhe",
-				form.current,
-				"user_GqWB6DWgQTHICnHQEnvCU"
-			)
-
-			.then(
-				(result) => {
-					console.log(result.text);
-					setSending(false);
-					setDisplayUserForm(false);
-				},
-				(error) => {
-					console.log(error.text);
-					setDisplayUserForm(false);
-				}
-			);
-	};
-
-	const displayUserFormHandler = () => {
-		setDisplayUserForm(true);
-	};
-
-	const URL = "https://wa.me";
-	let number = "+54 011 47162699";
-	number = number.replace(/[^\w\s]/gi, "").replace(/ /g, "");
-	let url = `${URL}/${number}`;
-
-	const enviarMsj = () => {
-		event.preventDefault();
-		url += `?text=${encodeURI(
-			"Hola, me interesaría tener mas información acerca de: " +
-				products.title +
-				" " +
-				" ( codigo de producto:  " +
-				products.code +
-				")"
-		)}`;
-
-		window.open(url);
-	};
+			setLoading(false);
+		}
+	}, [data]);
 
 	return (
 		<div className={styles.contAll}>
 			<Header />
-			<Hero2 title={products.title} color="white" />
-
+			{!products ? (
+				<Hero2 title={"Loading..."} color="white" />
+			) : (
+				<Hero2 title={products.title} color="white" />
+			)}
 			{loading ? (
 				""
 			) : (
@@ -104,13 +83,6 @@ const Index = () => {
 								<Loader />
 							</div>
 						)}
-						{/* <img
-							src={imageUrl}
-							className={styles.image_container}
-							style={loaded ? {} : { display: "none" }}
-							onLoad={() => setLoaded(true)}
-							alt="insumos medicos"
-						/> */}
 						<div className={styles.img_container}>
 							<img
 								src={products.img}
@@ -137,131 +109,15 @@ const Index = () => {
 							{products.techcnial}
 						</div>
 						<div className={styles.contact}>
-							<div className={styles.cotizacion}>
-								<h5>{products.title}</h5>
-								<p>{products.subtitle}</p>
-								<br />
-								<button onClick={enviarMsj}>Cotización via whatsapp</button>
-
-								<button
-									className={styles.btnMail}
-									style={{ background: "#8183CA" }}
-									onClick={displayUserFormHandler}
-								>
-									Cotización via mail
-								</button>
-=======
-import getProducts from "../api/getProducts";
-import Cotizacion from "../../components/Ui/Cotizacion/Cotizacion";
-
-export const getStaticPaths = async () => {
-  const data = await getProducts();
-  const paths = data.map((product) => {
-    return {
-      params: {
-        slug:
-          product[0].toString().toLowerCase().trim() ||
-          "ECG80".toString().toLowerCase().trim(),
-      },
-    };
-  });
-
-  return {
-    paths,
-    fallback: false,
-  };
-};
->>>>>>> 5fe8196aa9ec59822bab802ef164a60299e46c20
-
-export const getStaticProps = async (context) => {
-  const slug = context.params.slug;
-  const data = await getProducts();
-  const arrayFiltered = data.find(
-    (element) => element[0].toString().toLowerCase().trim() == slug
-  );
-  return {
-    props: { data: arrayFiltered },
-  };
-};
-
-const Index = ({ data }) => {
-  const [loaded, setLoaded] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const [products, setProducts] = useState(null);
-
-  useEffect(() => {
-    if (data) {
-      setProducts({
-        code: data[0],
-        category: !data[1] ? "" : data[1],
-        brand: data[2],
-        title: data[3],
-        brief: data[4],
-        description: data[5],
-        application: data[6],
-        techcnial: data[7],
-        img: !data[8]
-          ? "barbijo.png"
-          : "https://drive.google.com/uc?export=view&id=" + data[8],
-        ml: !data[9] ? "" : data[9],
-      });
-      setLoading(false);
-    }
-  }, [data]);
-
-  return (
-    <div className={styles.contAll}>
-      <Header />
-      {!products ? (
-        <Hero2 title={"Loading..."} color="white" />
-      ) : (
-        <Hero2 title={products.title} color="white" />
-      )}
-      {loading ? (
-        ""
-      ) : (
-        <div className={styles.container}>
-          <div className={styles.detail}>
-            {loaded ? null : (
-              <div className={styles.loader_container}>
-                <Loader />
-              </div>
-            )}
-            <div className={styles.img_container}>
-              <img
-                src={products.img}
-                alt="insumos medicos"
-                className={styles.image_container}
-                style={loaded ? {} : { display: "none" }}
-                onLoad={() => setLoaded(true)}
-                // alt="insumos medicos"
-              />
-            </div>
-            <div className={styles.detailInfo}>
-              <h4>Descripción</h4>
-              <br />
-              {products.description}
-              <br />
-              <br />
-              <h4>Aplicación</h4>
-              <br />
-              {products.application}
-              <br />
-              <br />
-              <h4>Ficha Técnica</h4>
-              <br />
-              {products.techcnial}
-            </div>
-            <div className={styles.contact}>
-              <Cotizacion
-                title={products.title}
-                subtitle={products.subtitle}
-                code={products.code}
-                merlibre={products.ml}
-              />
-            </div>
-          </div>
-          {/* <div className={styles.related}>
+							<Cotizacion
+								title={products.title}
+								subtitle={products.subtitle}
+								code={products.code}
+								merlibre={products.ml}
+							/>
+						</div>
+					</div>
+					{/* <div className={styles.related}>
 							<div className={styles.seccions}>
 								<div className={styles.swiper}>
 									<CarrouselMobile
@@ -271,14 +127,14 @@ const Index = ({ data }) => {
 								</div>
 							</div>
 						</div> */}
-        </div>
-      )}
+				</div>
+			)}
 
-      <div className={styles.footer}>
-        <Footer />
-      </div>
-    </div>
-  );
+			<div className={styles.footer}>
+				<Footer />
+			</div>
+		</div>
+	);
 };
 
 export default Index;
